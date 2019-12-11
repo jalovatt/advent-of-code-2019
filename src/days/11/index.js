36bc2db1396c452f07a56bfdadc5a1cf7889dc6c
Adam Lovatt <jalovatt@hotmail.com>
2019-12-11 07:23:20 -0700

    Tidies day 11

--- src/days/11/index.js
+++ src/days/11/index.js
@@ -23,28 +23,35 @@ class Robot {
 
   getInput = position => this.field[position] ?? 0;
 
+  updateComputer = (position) => {
+    const input = this.getInput(position);
+
+    const output = (this.computer.started)
+      ? this.computer.resume([input]).output.slice(-2)
+      : this.computer.execute(null, null, [input]).output.slice(-2);
+
+    this.log.push(output);
+
+    return (!output || this.computer.halted)
+      ? null
+      : output;
+  }
+
   execute = () => {
     this.log = [];
 
     while (!this.computer.halted) {
       const position = `${this.position[0]},${this.position[1]}`;
-      const input = this.getInput(position);
 
-      const output = (this.computer.started)
-        ? this.computer.resume([input]).output.slice(-2)
-        : this.computer.execute(null, null, [input]).output.slice(-2);
-
-      if (this.computer.halted) { break; }
+      const output = this.updateComputer(position);
+      if (!output) { break; }
 
       const [color, direction] = output;
 
-      this.log.push(output);
-
       if (this.field[position] !== color) {
+        this.field[position] = color;
         this.painted[position] = true;
       }
-
-      this.field[position] = color;
 
       this.direction = (this.direction + (direction ? 1 : -1) + 4) % 4;
 
@@ -54,19 +61,16 @@ class Robot {
 
   countPainted = () => Object.keys(this.painted).length;
 
-  print = () => {
-    const field = [];
+  print = () => Object.entries(this.field).reduce((acc, [key, color]) => {
+    if (!color) { return acc; }
 
-    Object.entries(this.field).forEach(([key, color]) => {
-      const [, x, y] = key.match(/([-\d]+),([-\d]+)/);
+    const [, x, y] = key.match(/([-\d]+),([-\d]+)/);
 
-      if (!field[y]) { field[y] = new Array(40).fill('⬛'); }
+    if (!acc[y]) { acc[y] = new Array(40).fill('⬛'); }
+    acc[y][x] = '⬜';
 
-      if (color) { field[y][x] = '⬜'; }
-    });
-
-    return field.map(row => row.map(v => v || '⬛').join('')).join('\n');
-  }
+    return acc;
+  }, []).map(row => row.join('')).join('\n');
 }
 
 export default Robot;
