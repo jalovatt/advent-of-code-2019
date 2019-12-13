4925af62659f23dc4638b316b684e9364dacb549
Adam Lovatt <jalovatt@hotmail.com>
2019-12-12 21:44:26 -0700

    Refactors day 12, adds LCM calculation

--- src/days/12/index.js
+++ src/days/12/index.js
@@ -1,55 +1,41 @@
-// TODO: Refactor to remove this
 /* eslint-disable no-param-reassign */
-
-// eslint-disable-next-line max-classes-per-file
-class Moon {
-  constructor(x, y, z) {
-    this.pos = { x, y, z };
-    this.vel = { x: 0, y: 0, z: 0 };
-  }
-
-  stringify = () => {
-    return `${this.pos.x},${this.pos.y},${this.pos.z},${this.vel.x},${this.vel.y},${this.vel.z}`;
-  }
-}
 
 class System {
   constructor(moons) {
     this.moons = moons.split('\n').map((str) => {
       const [, x, y, z] = str.match(/x=([-\d]+), y=([-\d]+), z=([-\d]+)/);
 
-      return new Moon(parseInt(x, 10), parseInt(y, 10), parseInt(z, 10));
+      return {
+        pos: { x: parseInt(x, 10), y: parseInt(y, 10), z: parseInt(z, 10) },
+        vel: { x: 0, y: 0, z: 0 },
+      };
     });
 
     this.initialState = {
-      x: this.stringifyAxisState('x'),
-      y: this.stringifyAxisState('y'),
-      z: this.stringifyAxisState('z'),
+      x: this.axisState('x'),
+      y: this.axisState('y'),
+      z: this.axisState('z'),
     };
 
     this.axisCycles = {};
   }
 
-  stringifyAxisState = (axis) => {
-    return this.moons.reduce((acc, moon) => {
-      acc.push(moon.pos[axis]);
-      acc.push(moon.vel[axis]);
+  axisState = axis => this.moons.reduce((acc, moon) => {
+    acc.push(moon.pos[axis]);
+    acc.push(moon.vel[axis]);
 
-      return acc;
-    }, []).join(',');
-  }
+    return acc;
+  }, []).join(',');
 
-  gravitate = (a, b) => {
-    Object.keys(a.vel).forEach((k) => {
-      if (a.pos[k] < b.pos[k]) {
-        a.vel[k] += 1;
-        b.vel[k] -= 1;
-      } else if (a.pos[k] > b.pos[k]) {
-        a.vel[k] -= 1;
-        b.vel[k] += 1;
-      }
-    });
-  }
+  gravitate = (a, b) => Object.keys(a.vel).forEach((k) => {
+    if (a.pos[k] < b.pos[k]) {
+      a.vel[k] += 1;
+      b.vel[k] -= 1;
+    } else if (a.pos[k] > b.pos[k]) {
+      a.vel[k] -= 1;
+      b.vel[k] += 1;
+    }
+  });
 
   updateVelocities = () => {
     for (let a = 0; a < (this.moons.length - 1); a += 1) {
@@ -72,57 +58,47 @@ class System {
       this.updateVelocities();
       this.updatePositions();
     }
+
+    return this;
   }
 
-  print = () => {
-    return this.moons.map((moon) => {
-      return `pos=<x=${moon.pos.x}, y=${moon.pos.y}, z=${moon.pos.z}>, vel=<x=${moon.vel.x}, y=${moon.vel.y}, z=${moon.vel.z}>`;
-    }).join('\n');
+  print = () => this.moons.map(moon => (
+    `pos=<x=${moon.pos.x}, y=${moon.pos.y}, z=${moon.pos.z}>, vel=<x=${moon.vel.x}, y=${moon.vel.y}, z=${moon.vel.z}>`
+  )).join('\n')
+
+  totalEnergy = () => this.moons.reduce((acc, moon) => {
+    const p = Math.abs(moon.pos.x) + Math.abs(moon.pos.y) + Math.abs(moon.pos.z);
+    const k = Math.abs(moon.vel.x) + Math.abs(moon.vel.y) + Math.abs(moon.vel.z);
+
+    return acc + p * k;
+  }, 0)
+
+  findAxisCycles = (step) => {
+    ['x', 'y', 'z'].forEach((axis) => {
+      if (!this.axisCycles[axis] && this.axisState(axis) === this.initialState[axis]) {
+        this.axisCycles[axis] = step;
+      }
+    });
+
+    return (this.axisCycles.x && this.axisCycles.y && this.axisCycles.z);
   }
 
-  totalEnergy = () => {
-    return this.moons.reduce((acc, moon) => {
-      const p = Math.abs(moon.pos.x) + Math.abs(moon.pos.y) + Math.abs(moon.pos.z);
-      const k = Math.abs(moon.vel.x) + Math.abs(moon.vel.y) + Math.abs(moon.vel.z);
+  // Inefficient (takes >1 billion loops for the solution), but at least I understand it...
+  findOverallCycle = () => {
+    const [m, n, max] = Object.values(this.axisCycles).sort((a, b) => a - b);
+    const product = m * n * max;
 
-      return acc + p * k;
-    }, 0);
-  }
-
-  checkAxisCycles = (step) => {
-    if (!this.axisCycles.x && (this.stringifyAxisState('x') === this.initialState.x)) {
-      console.log(`found a cycle for x at ${step}`);
-      this.axisCycles.x = step;
-    }
-    if (!this.axisCycles.y && (this.stringifyAxisState('y') === this.initialState.y)) {
-      console.log(`found a cycle for y at ${step}`);
-      this.axisCycles.y = step;
-    }
-    if (!this.axisCycles.z && (this.stringifyAxisState('z') === this.initialState.z)) {
-      console.log(`found a cycle for z at ${step}`);
-      this.axisCycles.z = step;
+    for (let i = max; i <= product; i += max) {
+      if (i % n === 0 && i % m === 0) {
+        return i;
+      }
     }
 
-    if (this.axisCycles.x && this.axisCycles.y && this.axisCycles.z) {
-      console.log('axis cycles!!!!!');
-
-      return this.axisCycles.x * this.axisCycles.y * this.axisCycles.z;
-    }
-  }
-
-  // TODO: Implement a LCM algorithm for this rather than using an internet app
-  findTotalCycle = () => {
-    console.dir(this.axisCycles);
-    return Object.values(this.axisCycles).reduce((acc, cur) => acc * cur);
+    return -1;
   }
 }
 
-export const simulateMoons = (moons, n) => {
-  const system = new System(moons);
-  system.simulate(n);
-
-  return system;
-};
+export const simulateMoons = (moons, n) => new System(moons).simulate(n);
 
 export const findCycleTime = (moons) => {
   const system = new System(moons);
@@ -133,8 +109,7 @@ export const findCycleTime = (moons) => {
     if (n > MAX_CYCLES) { console.error(`Halted after ${MAX_CYCLES} steps`); return -1; }
     system.simulate();
     n += 1;
-  } while (!system.checkAxisCycles(n));
+  } while (!system.findAxisCycles(n));
 
-  console.log(`found all cycles at ${n - 1}`);
-  return system.findTotalCycle();
+  return system.findOverallCycle();
 };
