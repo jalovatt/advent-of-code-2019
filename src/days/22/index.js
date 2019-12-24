25a9728953367afd74922cb725e96744654a16b4
Adam Lovatt <jalovatt@hotmail.com>
2019-12-23 20:28:45 -0700

    Begins working on day 22.2

--- src/days/22/index.js
+++ src/days/22/index.js
@@ -37,8 +37,70 @@ const parseInstructions = (instructions) => {
   });
 };
 
+const SIZE = 119315717514047;
+const ITERATIONS = 101741582076661;
+
+const reversePos = (pos) => SIZE - 1 - pos;
+const cachedInstructions = {
+  'deal into new stack': reversePos,
+};
+
+// .reduce((acc, cur) => { acc.unshift(cur); return acc; }, [])
+const parseInstructions2 = (instructions) => {
+  return instructions.split('\n').map((line => {
+    if (cachedInstructions[line]) { return cachedInstructions[line]; }
+    if (line.match('cut')) {
+
+      let n = parseInt(line.match(/([-\d]+)$/)[1], 10);
+      cachedInstructions[line] = (card) => {
+        if (n < 0) { n = SIZE - n; }
+        if (card >= n) {
+          return card - n;
+        }
+
+        return card + (SIZE - 1 - n);
+      };
+
+      return cachedInstructions[line];
+    }
+
+    const n = parseInt(line.match(/([-\d]+)$/)[1], 10);
+    cachedInstructions[line] = (card) => {
+      return (card * n) % SIZE;
+    };
+
+    return cachedInstructions[line];
+  }));
+};
+
+const findLoopSize = (parsed, card) => {
+  let n = 0;
+  let cur = card;
+
+  do {
+    if (n === 1000) { throw new Error('hit limit'); }
+    cur = parsed.reduce((acc, cur) => cur(acc), cur);
+    console.log(cur);
+    n += 1;
+  } while (cur !== card);
+
+  return n;
+};
+
 export const part1 = (instructions, size) => {
   const deck = new Array(size).fill(null).map((v, i) => i);
   const parsed = parseInstructions(instructions);
   return parsed.reduce((acc, cur) => cur(acc), deck);
+};
+
+export const part2 = (instructions, card) => {
+  const parsed = parseInstructions2(instructions, card);
+
+  const loopSize = findLoopSize(parsed, card);
+  // const needed = ITERATIONS % loopSize;
+
+  // for (let i = 0; i < needed; i += 1) {
+  //   card = parsed.reduce((acc, cur) => cur(acc), card);
+  // }
+  console.log(loopSize);
 };
