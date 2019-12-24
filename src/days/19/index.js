47b86ae2b4ff67a987d4dbc00d11b32ef1db7fdd
Adam Lovatt <jalovatt@hotmail.com>
2019-12-23 21:57:32 -0700

    Continues working on 19.2

--- src/days/19/index.js
+++ src/days/19/index.js
@@ -1,31 +1,12 @@
 import IntCode from '../../common/IntCode';
-import loadText from '../../utilities/loadText';
-
-const program = loadText('input.txt');
 
 const printField = (field) => {
   // eslint-disable-next-line no-console
   console.log(field.map(row => row.map(cell => (cell ? '#' : '.')).join('')).join('\n'));
 };
 
-const findFitCoords = (field, targetSize) => {
-  for (let y = (targetSize * 2); y < field.length; y += 1) {
-    const row = field[y];
-    if (row.filter(c => !!c).length >= targetSize) {
-      const rightCol = row.lastIndexOf(1);
-
-      if (field[y + targetSize - 1]?.[rightCol - targetSize + 1] === 1) {
-        const x = field[y + targetSize - 1].indexOf(1);
-        return { x, y };
-      }
-    }
-  }
-
-  throw new Error(`This field can't fit a ${targetSize}-unit square`);
-};
-
 class TractorBeam {
-  constructor(n) {
+  constructor(program, n) {
     this.n = n;
     this.computer = new IntCode(program);
     this.inputPositions = new Array(n * n).fill(null).map((_, i) => [i % n, Math.floor(i / n)]);
@@ -53,8 +34,8 @@ class TractorBeam {
   }
 }
 
-export const part1 = (fieldSize) => {
-  const tb = new TractorBeam(fieldSize);
+export const part1 = (program, fieldSize) => {
+  const tb = new TractorBeam(program, fieldSize);
   const field = tb.execute();
   return tb.countPoints(field);
 };
@@ -66,9 +47,80 @@ export const part2test = (field, targetSize) => {
   // return tb.fit(field, targetSize);
 };
 
-export const part2 = () => {
-  const tb = new TractorBeam(300);
-  const field = tb.execute();
-  const coords = findFitCoords(field, 100);
-  return [coords.x, coords.y, (coords.x * 10000 + coords.y)];
+const findFitCoords = (field, targetSize) => {
+  for (let y = (targetSize * 2); y < field.length; y += 1) {
+    const row = field[y];
+    if (row.filter(c => !!c).length >= targetSize) {
+      const rightCol = row.lastIndexOf(1);
+
+      if (field[y + targetSize - 1]?.[rightCol - targetSize + 1] === 1) {
+        const x = field[y + targetSize - 1].indexOf(1);
+        return { x, y };
+      }
+    }
+  }
+
+  throw new Error(`This field can't fit a ${targetSize}-unit square`);
+};
+
+export const part2 = (program, TARGET_SIZE = 100) => {
+  const computer = new IntCode(program);
+
+  const shipWillFitAt = (x, y) => (
+    computer.execute(null, null, [x, y]).output[0]
+    && computer.execute(null, null, [x + TARGET_SIZE - 1, y]).output[0]
+    && computer.execute(null, null, [x, y + TARGET_SIZE - 1]).output[0]
+  );
+
+  // To fit the example input
+  const SLOPE_CHECK = 33;
+  let slopeLower;
+  {
+    let x = 0;
+    while (!slopeLower) {
+      if (computer.execute(null, null, [x, SLOPE_CHECK]).output[0]) { slopeLower = SLOPE_CHECK / x; }
+      x += 1;
+    }
+  }
+
+  let slopeUpper;
+  {
+    let y = 0;
+    while (!slopeUpper) {
+      if (computer.execute(null, null, [SLOPE_CHECK, y]).output[0]) { slopeUpper = y / SLOPE_CHECK; }
+      y += 1;
+    }
+  }
+
+  const slope = (slopeUpper + slopeLower) / 2;
+
+  let point;
+  let x = TARGET_SIZE;
+  while (!point) {
+    const y = Math.floor(x * slope);
+    if (shipWillFitAt(x, y)) { point = { x, y }; }
+    x += 1;
+  }
+
+  while (true) {
+    const possible = [
+      { x: point.x - 1, y: point.y },
+      { x: point.x - 1, y: point.y - 1 },
+      { x: point.x, y: point.y - 1 },
+      { x: point.x + 1, y: point.y - 1 },
+      { x: point.x - 1, y: point.y + 1},
+    ]
+      .map(pos => ({ ...pos, fit: shipWillFitAt(pos.x, pos.y) }));
+
+    // console.log('----------');
+    // console.log(point);
+    // console.log(JSON.stringify(possible, null, 2));
+
+    const willFit = possible.filter(pos => !!pos.fit);
+    if (!willFit.length) { break; }
+
+    [point] = willFit.sort((a, b) => (a.x ** 2 + a.y ** 2) - (b.x ** 2 + b.y ** 2));
+  }
+
+  return [point.x, point.y, (point.x * 10000 + point.y)];
 };
